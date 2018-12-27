5e8be5f5f12f2fa98cce9fa6903021be5262ffe2
UMCU imedia <umcu.imedia@gmail.com>
2018-12-27 15:11:41 +0100

    1.2 push

---
+++ onservertest4Tests/onservertest4Tests.swift
@@ -0,0 +1,34 @@
+//
+//  onservertest4Tests.swift
+//  onservertest4Tests
+//
+//  Created by Rens Wiebenga on 27/12/2018.
+//  Copyright © 2018 UMC Utrecht. All rights reserved.
+//
+
+import XCTest
+@testable import onservertest4
+
+class onservertest4Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
